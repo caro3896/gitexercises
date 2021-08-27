6c01a731860e2b54c25a247400007f26fe503a74
Caroline <caro713f@stud.kea.dk>
2021-08-27 09:27:10 +0200

    variables exercise

---
+++ variables.js
@@ -0,0 +1,31 @@
+"use strict";
+const bool = "true";
+const num = 41;
+const str = "Peter";
+const obj = {
+    cats: 2,
+    cars: 1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+// See value of the variable i the console
+console.log(bool);
+console.log(num);
+console.log(str);
+console.log(obj);
+console.log(nothing);
+console.log(undf);
+console.log(symbol);
+ 
+// See type of variable - write in this in console
+// typeof bool;
+// typeof num;
+// typeof str;
+// typeof obj;
+// typeof nothing;
+// typeof undf;
+// typeof symbol;
+
+ 

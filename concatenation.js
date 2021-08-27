19aa8465705b432931c107f96ca1573623832b7e
Caroline <caro713f@stud.kea.dk>
2021-08-27 10:04:36 +0200

    concatenation exercise

---
+++ concatenation.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+console.log(n1 + n2); // 3
+console.log(s1 + s2); // 12
+
+console.log(n1 + s2); // 12
+console.log(s1 + n2);

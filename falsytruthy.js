56d041de4d49213d4ebb06fbece81df2eb934345
Caroline <caro713f@stud.kea.dk>
2021-08-27 10:39:09 +0200

    falsytruthy exercise

---
+++ falsytruthy.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const value = " ";
+
+if (value){
+    console.log("Value is truthy");
+} else {
+    console.log("Value is falsy");
+}
+
+// true - true
+// false - false
+// null - false
+// undefined - false 
+// 0 - false
+// "0" - true
+// "" - false
+// "true" - true
+// "false" - true
+// 2 - true
+// {} - true
+// [] - true
+// NaN - false
+// Infinity - true
+// 3.14 - true
+// -0 - false
+// " "

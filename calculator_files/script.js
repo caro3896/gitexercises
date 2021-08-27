40de56e9d6db203a46e12cdb78c4fa2d1cf5c216
Caroline <caro713f@stud.kea.dk>
2021-08-27 11:49:11 +0200

    functions set up

--- calculator_files/script.js
+++ calculator_files/script.js
@@ -1,1 +1,77 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", settingUp());
+
+function settingUp() {
+console.log("settingUp");
+clickCalculate();
+}
+
+function clickCalculate(){
+console.log("clickCalculate");
+readFirstNumber();
+}
+
+function readFirstNumber(){
+console.log("readFirstNumber");
+readSecondNumber();
+}
+
+function readSecondNumber() {
+console.log("readSecondNumber");
+readOperator();
+}
+
+function readOperator(){
+console.log("readOperator");
+calculatePlus();
+}
+
+function calculatePlus(){
+console.log("calculatePlus");
+checkRounding();
+}
+
+function calculateMinus(){
+    console.log("calculateMinus");
+    checkRounding();
+}
+
+function calculateMultiply(){
+    console.log("calculateMultiply");
+    checkRounding();
+}
+
+function calculateDivide(){
+    console.log("calculateDivide");
+    checkRounding();
+}
+
+function checkRounding(){
+    console.log("checkRounding");
+    readDecimals();
+}
+
+function readDecimals(){
+    console.log("readDecimals");
+    roundResult();
+}
+
+function roundResult(){
+    console.log("roundResult");
+    writeResult();
+}
+
+function writeResult(){
+    console.log("writeResult");
+    appendResult();
+}
+
+function appendResult(){
+    console.log("appendResult");
+    scroll();
+}
+
+function scroll(){
+    console.log("scroll");
+}

531ac2ec5b3efc7c85d2035aeca4fd5a3be9e939
Caroline <caro713f@stud.kea.dk>
2021-08-29 12:26:33 +0200

    different calculations work

--- calculator_files/script.js
+++ calculator_files/script.js
@@ -2,8 +2,7 @@
 
 let firstNumber = document.querySelector("#firstnumber").value;
 let secondNumber = document.querySelector("#secondnumber").value;
-let operator = document.querySelector("#operator");
-let operatorValue = operator.options[operator.selectedIndex].value;
+let operator = document.querySelector("#operator").value;
 let answer = document.querySelector("#results li");
 
 
@@ -26,38 +25,43 @@ readOperator();
 }
 
 function readOperator(){
-console.log("Operator:", operatorValue);
-// calculatePlus();
-if (operatorValue === "add")
-    {
+console.log("Operator:", operator);
+if (operator === 'add') {
     calculatePlus();
-} else if (operatorValue === 'sub') {
+} else if (operator === 'sub') {
     calculateMinus();
-} else if (operatorValue === 'mul') {
+} else if (operator === 'mul') {
     calculateMultiply();
 } else {
     calculateDivide();
 }
 }
 
-function calculatePlus(){
-let plus = parseInt(firstNumber)+parseInt(secondNumber);
-answer.innerHTML = plus;
-checkRounding();
+function calculatePlus(){ 
+    let plus = parseInt(firstNumber)+parseInt(secondNumber);
+    console.log("Plus: ", plus);
+    answer.innerHTML = plus;
+    checkRounding();
 }
 
 function calculateMinus(){
-    console.log("calculateMinus");
+    let minus = parseInt(firstNumber)-parseInt(secondNumber);
+    console.log("Minus: ", minus);
+    answer.innerHTML = minus;
     checkRounding();
 }
 
 function calculateMultiply(){
-    console.log("calculateMultiply");
+    let multi = parseInt(firstNumber)*parseInt(secondNumber);
+    console.log("Multiply: ", multi);
+    answer.innerHTML = multi;
     checkRounding();
 }
 
 function calculateDivide(){
-    console.log("calculateDivide");
+    let div = parseInt(firstNumber)/parseInt(secondNumber);
+    console.log("Divide: ", div);
+    answer.innerHTML = div;
     checkRounding();
 }
 

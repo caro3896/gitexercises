679dda798cf45ad1e2732b4673ff2bec72acf9a8
Caroline <caro713f@stud.kea.dk>
2021-08-31 12:54:11 +0200

    capitalization update

--- capitalization3.js
+++ capitalization3.js
@@ -1,6 +1,6 @@
 "use strict";
 let str = "peter";
 
-let newStr = str.substring(0,2).toLowerCase() + str[2].toUpperCase() + str.substring(3).toLowerCase();
+let newStr = str.substring(0,2).toLowerCase() + str.substring(2,3).toUpperCase() + str.substring(3).toLowerCase();
 
 console.log(newStr);

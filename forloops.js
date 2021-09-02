b1a6cd37767b873c5c3b077d42b6ea2e0e191be3
Caroline <caro713f@stud.kea.dk>
2021-09-02 12:53:31 +0200

    forloops exercise

---
+++ forloops.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// console.log from 0 to 9
+// for(let counter=0; counter < 10; counter++){
+//     console.log(counter);
+// }
+
+// console.log from 1 to 10
+// for(let counter=1; counter < 11; counter++){
+//     console.log(counter);
+// }
+
+// console.log from 10 to 0, after 0 console.log "liftoff"
+// for(let counter=10; counter >= -1; counter--){
+//     console.log(counter);
+//     if (counter < 0) {
+//         console.log("Liftoff");
+//     }
+//     }
+
+// console.log from 1 to 19, only count odd numbers
+// for(let counter=1; counter < 20; counter+=2){
+//     console.log(counter);
+//     }
+
+// console.log from 1 to 16777216, doubling each time
+// for(let counter=1; counter <= 16777216; counter+=counter){
+//     console.log(counter);
+//     }
+
+// console.log from 111 to 138, in steps of 3
+// for(let counter=111; counter <= 138; counter+=3){
+//     console.log(counter);
+//     }
+
+// console.log from 100 down to 0, in steps of 10
+for(let counter=100; counter >= 0; counter-=10){
+    console.log(counter);
+    }

5a076baa3de237949db1dc98dbfedf30bd4227c4
Caroline <caro713f@stud.kea.dk>
2021-09-06 14:20:11 +0200

    parameters exercise

---
+++ param.js
@@ -0,0 +1,13 @@
+"uses strict";
+
+// sayHello(myName);
+
+// function sayHello(firstName) {
+// console.log (`Hello ${firstName}`)
+// }
+
+presentPet("Caroline", "cat", "Loui");
+
+function presentPet(firstName, animalType, animalName){
+ console.log(`My name is ${firstName}, I have a ${animalType} named ${animalName}`);
+}
